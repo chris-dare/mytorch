51855214ab3264aa1eb1a4ad2e1fb851105ab112
Chris Dare <cdare@andrew.cmu.edu>
2020-09-28 03:06:00 +0200

    Run black, isort

--- mytorch/nn/linear.py
+++ mytorch/nn/linear.py
@@ -1,25 +1,26 @@
 import numpy as np
-
+from mytorch.nn.functional import MatMul
 from mytorch.nn.module import Module
 from mytorch.tensor import Tensor
 
 
 class Linear(Module):
     """A linear layer (aka 'fully-connected' or 'dense' layer)
-    
+
     >>> layer = Linear(2,3)
     >>> layer(Tensor.ones(10,2)) # (batch_size, in_features)
     <some tensor output with size (batch_size, out_features)>
-    
+
     Args:
         in_features (int): # dims in input
                            (i.e. # of inputs to each neuron)
         out_features (int): # dims of output
                            (i.e. # of neurons)
-        
+
     Inherits from:
         Module (mytorch.nn.module.Module)
     """
+
     def __init__(self, in_features, out_features):
         super().__init__()
 
